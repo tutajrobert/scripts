e3bbad5ffed7ac4d093808f406a4512e8f6914a5
tutaj robert <tutajrobert@gmail.com>
2017-03-08 22:59:34 +0100

    No one likes databases!

--- phonebook.py
+++ phonebook.py
@@ -1,25 +1,64 @@
 import sqlite3
 
-connection = sqlite3.connect(":memory:")
-cursor = connection.cursor()
+class Phonebook():
+    def __init__(self):
+        self.con = sqlite3.connect(":memory:")
+        self.cur = self.con.cursor() 
+                                                
+        self.cur.execute("CREATE TABLE pb_contact(contact_id INTEGER PRIMARY KEY, \
+                                                  contact_name TEXT)")
+        self.cur.execute("CREATE TABLE pb_number(number_id INTEGER PRIMARY KEY, \
+                                                 number_string TEXT, \
+                                                 contact_name TEXT, \
+                                                 FOREIGN KEY(contact_name) \
+                                                     REFERENCES pb_contact(contact_id))")
+    def add_contact(self, contact_name):
+        if contact_name in self.check_contact():
+            print("CONTACT: {} is already in phonebook".format(contact_name))
+            return False
+        contact_sql_string = "INSERT INTO pb_contact(contact_name) VALUES ('{}')".format(contact_name)
+        self.cur.execute(contact_sql_string)
+        print("Succesfully added CONTACT: {}".format(contact_name))
 
-cursor.execute("CREATE TABLE pb_group(group_id INTEGER PRIMARY KEY, group_name TEXT)")
-cursor.execute("INSERT INTO pb_group(group_name) VALUES ('Family')")
-cursor.execute("INSERT INTO pb_group(group_name) VALUES ('Friends')")
-cursor.execute("INSERT INTO pb_group(group_name) VALUES ('Others')")
-cursor.execute("SELECT * FROM pb_group")
-print(cursor.fetchall())
+    def show_contact(self):
+        self.cur.execute("SELECT * FROM pb_contact")
+        print(self.cur.fetchall())
+        
+    def check_contact(self):
+        self.cur.execute("SELECT * FROM pb_contact")
+        contact_list = self.cur.fetchall()
+        contact_names = [i[1] for i in contact_list]
+        return contact_names
+        
+    def add_number(self, number_string, contact_name):
+        if number_string in self.check_number():
+            print("NUMBER: {} is already in phonebook".format(number_string))
+            return False
+        elif contact_name not in self.check_contact():
+            print("There is no CONTACT: {} in phonebook".format(contact_name),
+                  "\nNumber was not added to phonebook",
+                  "\nPossible CONTACTS are: {}".format(self.check_contact()))
+            return False
+        number_sql_string = "INSERT INTO pb_number VALUES (null, '{}', '{}')".format(number_string, contact_name)
+        self.cur.execute(number_sql_string)
+        print("Succesfully added NUMBER: {} to CONTACT: {}".format(number_string, contact_name))
+    
+    def check_number(self):
+        self.cur.execute("SELECT * FROM pb_number")
+        number_list = self.cur.fetchall()
+        number_strings = [i[1] for i in number_list]
+        return number_strings
+                        
+    def show_number(self):    
+        self.cur.execute("SELECT * FROM pb_number")
+        print(self.cur.fetchall())       
 
-cursor.execute("CREATE TABLE pb_contact(contact_id INTEGER PRIMARY KEY, contact_name TEXT, group_name TEXT, FOREIGN KEY(group_name) REFERENCES pb_group(group_id))")
-cursor.execute("INSERT INTO pb_contact VALUES (null, 'Customer Service', 'Others')")
-cursor.execute("INSERT INTO pb_contact VALUES (null, 'Taxi Duck', null)")
-cursor.execute("SELECT * FROM pb_contact")
-print(cursor.fetchall())
-
-cursor.execute("CREATE TABLE pb_number(number_id INTEGER PRIMARY KEY, number_string INT, contact_name TEXT, FOREIGN KEY(contact_name) REFERENCES pb_contact(contact_id))")
-cursor.execute("INSERT INTO pb_number VALUES (null, '625-800-688', 'Customer Service')")
-cursor.execute("INSERT INTO pb_number VALUES (null, '714-295-123', 'Customer Service')")
-cursor.execute("SELECT * FROM pb_number WHERE contact_name='Customer Service'")
-print(cursor.fetchall())
-
-cursor.close()
+pb = Phonebook()
+pb.add_contact("Freddie Mercury")
+pb.add_contact("Freddie Mercury")
+pb.show_contact()
+pb.add_number("12123412", "Jon Snow")
+pb.add_number("600900900", "Freddie Mercury")
+pb.add_number("600900900", "Jon Snow")
+pb.show_number()
+pb.show_contact()
