b2006398e2c3f4fc50090cae86e48dbc70ca46b9
tutaj robert <tutajrobert@gmail.com>
2017-03-10 10:46:12 +0100

    And now my watch begins

--- phonebook.py
+++ phonebook.py
@@ -1,7 +1,21 @@
 import sqlite3
 
+"""
+Simple CRUD application in python using SQLite (sqlite3) package.
+Author: Robert Konieczny
+Homework on the subject Databases and data warehouses
+"""
+
 class Phonebook():
+    """
+    Phonebook database contains two tables: pb_contact, pb_number in which contacts and numbers are stored.
+    One contact name can have multiple phone numbers. Contacts and numbers can't be repeated.
+    pb_number takes as foreign key contact_name from the pb_contact table - "one to many" relation.
+	Almost all functions return False - it is needed for command line GUI operation.
+    """
+	
     def __init__(self):
+	#Initializing database running "in memory" and creating two tables: pb_contact, pb_number
         self.con = sqlite3.connect(":memory:")
         self.cur = self.con.cursor() 
                                                 
@@ -12,7 +26,9 @@ class Phonebook():
                                                  contact_name TEXT, \
                                                  FOREIGN KEY(contact_name) \
                                                      REFERENCES pb_contact(contact_id))")
+							
     def add_contact(self, contact_name):
+	#Adds new contact to pb_contact. It stops when contact_name is already in table
         if contact_name in self.check_contact():
             print("CONTACT: {} is already in phonebook".format(contact_name))
             return True
@@ -26,12 +42,15 @@ class Phonebook():
         print(self.cur.fetchall())
         
     def check_contact(self):
+	#Checks if contact is in table pb_contact
         self.cur.execute("SELECT * FROM pb_contact")
         contact_list = self.cur.fetchall()
         contact_names = [i[1] for i in contact_list]
         return contact_names
 		
     def delete_contact(self, contact_name, delete_message = True):
+	#Deletes contact from pb_contact and all numbers related to this contact from pb_number
+	#Stops when wrong contact is given
         if contact_name not in self.check_contact():
             print("There is no such CONTACT: {} in phonebook".format(contact_name))
             return False
@@ -45,6 +64,8 @@ class Phonebook():
         return False
         
     def update_contact(self, old_contact_name, new_contact_name):
+	#Updates contact name to new name and reasign all related numbers to a new contact
+	#Stops when wrong contact is given or new contact name is already in pb_contact
         if old_contact_name not in self.check_contact():
             print("There is no such CONTACT: {} in phonebook".format(old_contact_name))
             return False
@@ -60,6 +81,8 @@ class Phonebook():
         return False
 
     def add_number(self, number_string, contact_name):
+	#Adds number to pb_number with its relation to contact from pb_contact
+	#Stops when number is already in pb_number or wrong contact name is given
         if number_string in self.check_number():
             print("NUMBER: {} is already in phonebook".format(number_string))
             return False
@@ -74,12 +97,15 @@ class Phonebook():
         return False
     
     def check_number(self):
+	#Checks if number is in table pb_number
         self.cur.execute("SELECT * FROM pb_number")
         number_list = self.cur.fetchall()
         number_strings = [i[1] for i in number_list]
         return number_strings
         
     def contact_with_no_number(self, contact_name):
+	#Checks if contact has only one number assigned
+	#Needed for delete number procedure - one can't delete the last number from contact without deleting a contact first
         self.cur.execute("SELECT number_string FROM pb_number WHERE contact_name='{}'".format(contact_name))
         number_list = self.cur.fetchall()
         if len(number_list) == 0:
@@ -92,6 +118,8 @@ class Phonebook():
         print(self.cur.fetchall())
 
     def delete_number(self, number_string, delete_contact = False):
+	#Deletes number from pb_number
+	#Stops when wrong number is given or if it last number of contact
         if number_string not in self.check_number():
             print("There is no such NUMBER: {} in phonebook".format(number_string))
             return False
@@ -108,6 +136,7 @@ class Phonebook():
         return False
 
     def update_number(self, old_number_string, new_number_string):
+	#Updates number. Stops when wrong number is given or new number is already in pb_number
         if old_number_string not in self.check_number():
             print("There is no such NUMBER: {} in phonebook".format(old_number_string))
             return False
@@ -119,6 +148,8 @@ class Phonebook():
         return False
 
     def reasign_number(self, number_string, contact_name, edit_contact = False):
+	#Updates number - assign it to different contact
+	#stops when wrong number is given, wrong contact is given or it is last number of contact
         if number_string not in self.check_number():
             print("There is no such NUMBER: {} in phonebook".format(number_string))
             return False        
@@ -149,8 +180,13 @@ class Phonebook():
         print("\nPHONEBOOK ENDS HERE")
         return False
 
+"""
+Phonebook application starts here
+"""
+		
 pb = Phonebook()
 
+#Creating example phonebook for testing
 print("\nExemplary phonebook will be created for project presentation purposes only.\n\
 Terminal outputs below are results of written functions execution.\n")
 pb.add_contact("Freddie Mercury")
@@ -175,13 +211,15 @@ def menu():
     4 - Delete number\n\
     5 - Update contact into new contact name\n\
     6 - Update number into new one\n\
-    7 - Reasign existing number into different contact\n\
+    7 - Reassign existing number into different contact\n\
     8 - Exit phonebook application\n")
     selection = input("Choose an option: ")
     print("-------------------------------------")
     return selection
 
 def selection_event(pb, sel):
+    #Simple terminal GUI selector
+	#All selection, but "8 - Exit phonebook application" returns False
     if sel == "0":
         stop = pb.show_phonebook()
         return stop
@@ -227,6 +265,13 @@ def selection_event(pb, sel):
     elif sel == "8":
         print("\nBye!\n")
         return True
+    else:
+        print("\nWrong input! Please try again.")
+        return False
+
+"""
+Run a phonebook application in while loop
+"""
 
 stop = False
 while stop is False:     
